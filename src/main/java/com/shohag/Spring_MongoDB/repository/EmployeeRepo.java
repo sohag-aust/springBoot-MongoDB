f894ffb5b1b6b9327ebc5ef88769c0a4c5a649e6
sohag-aust <frustratedcoder96@gmail.com>
2022-10-20 14:38:45 +0600

    refactor

--- src/main/java/com/shohag/Spring_MongoDB/repository/EmployeeRepo.java
+++ src/main/java/com/shohag/Spring_MongoDB/repository/EmployeeRepo.java
@@ -1,41 +1,7 @@
 package com.shohag.Spring_MongoDB.repository;
 
 import com.shohag.Spring_MongoDB.model.Employee;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
-@Repository
-public class EmployeeRepo {
-
-    private MongoTemplate mongoTemplate;
-
-    public EmployeeRepo(MongoTemplate mongoTemplate) {
-        this.mongoTemplate = mongoTemplate;
-    }
-
-    public Employee save(Employee employee) {
-        return mongoTemplate.save(employee);
-    }
-
-    public List<Employee> findAll() {
-        return mongoTemplate.findAll(Employee.class);
-    }
-
-    public long delete(Employee employee) {
-        return mongoTemplate.remove(employee).getDeletedCount();
-    }
-
-    public List<Employee> getBySalary(int salary) {
-        Query query = new Query(Criteria.where("salary").gte(salary));
-        return mongoTemplate.find(query, Employee.class);
-    }
-
-    public List<Employee> getByFirstName(String firstName) {
-        Query query = new Query(Criteria.where("firstName").regex("^"+firstName));
-        return mongoTemplate.find(query, Employee.class);
-    }
+public interface EmployeeRepo extends MongoRepository<Employee, String> {
 }
